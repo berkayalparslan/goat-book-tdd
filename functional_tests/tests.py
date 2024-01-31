07d2e2638627a1781cf0815874c04cafc5df6376
Berkay Alparslan <15653983+berkayalparslan@users.noreply.github.com>
2024-02-01 00:38:32 +0100

    make functional_tests an app, use LiveServerTestCase

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,9 +1,10 @@
 import unittest, time
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
 
@@ -18,7 +19,7 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_todo_list(self):
         #edith heard about a cool new online to-do app
         #she goes to check out its homepage
-        self.browser.get("http://localhost:8000")
+        self.browser.get(self.live_server_url)
 
         #she notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
