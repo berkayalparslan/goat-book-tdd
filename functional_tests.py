b8d6b29d10c421d778101fc35d278cb57136dcb9
Berkay Alparslan <15653983+berkayalparslan@users.noreply.github.com>
2024-01-31 21:22:24 +0100

    refactored functional tests

--- functional_tests.py
+++ functional_tests.py
@@ -9,6 +9,11 @@ class NewVisitorTest(unittest.TestCase):
 
     def tearDown(self) -> None:
         self.browser.quit()
+    
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element(By.ID, 'id_list_table')
+        rows = table.find_elements(By.TAG_NAME, 'tr')
+        self.assertIn(row_text, [row.text for row in rows])
 
     def test_can_start_a_todo_list(self):
         #edith heard about a cool new online to-do app
@@ -21,25 +26,20 @@ class NewVisitorTest(unittest.TestCase):
         self.assertIn("To-Do", header_text)
 
         # She is invited to enter a to-do item straight away
-        inputBox = self.browser.find_element(By.ID,"id_new_item")
-        self.assertEqual(inputBox.get_attribute("placeholder"), "Enter a to-do item")
+        inputbox = self.browser.find_element(By.ID,"id_new_item")
+        self.assertEqual(inputbox.get_attribute("placeholder"), "Enter a to-do item")
 
         # She types "Buy peacock feathers" into a text box
         # (Edith's hobby is tying fly-fishing lures)
-        inputBox.send_keys("Buy peacock feathers")
+        inputbox.send_keys("Buy peacock feathers")
 
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list
-        inputBox.send_keys(Keys.ENTER)
+        inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
+        self.check_for_row_in_list_table("1: Buy peacock feathers")
 
-        # The page updates again, and now shows both items on her list
-        table = self.browser.find_element(By.ID, 'id_list_table')
-        rows = table.find_elements(By.TAG_NAME, 'tr')
-        self.assertIn("1: Buy peacock feathers", [row.text for row in rows])
-
-        #blabla
-        inputbox = self.browser.find_element(By.ID, "id_new_item")
+        inputbox = self.browser.find_element(By.ID,"id_new_item")
         inputbox.send_keys("Use peacock feathers to make a fly")
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
@@ -47,16 +47,8 @@ class NewVisitorTest(unittest.TestCase):
         # The page updates again, and now shows both items on her list
         table = self.browser.find_element(By.ID, "id_list_table")
         rows = table.find_elements(By.TAG_NAME, "tr")
-        self.assertIn(
-            "1: Buy peacock feathers",
-            [row.text for row in rows],
-        )
-        self.assertIn(
-            "2: Use peacock feathers to make a fly",
-            [row.text for row in rows],
-        )
-
-
+        self.check_for_row_in_list_table("1: Buy peacock feathers")
+        self.check_for_row_in_list_table("Use peacock feathers to make a fly")
 
         # Satisfied, she goes back to sleep
 
