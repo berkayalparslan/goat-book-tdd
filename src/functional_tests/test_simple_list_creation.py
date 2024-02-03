ed312f515723853c59d7fdfcdc3202b17d517cf4
Berkay Alparslan <15653983+berkayalparslan@users.noreply.github.com>
2024-02-03 18:27:46 +0100

    moved FTs into their own individual files

--- src/functional_tests/test_simple_list_creation.py
+++ src/functional_tests/test_simple_list_creation.py
@@ -1,37 +1,9 @@
-import unittest, time, os
-from django.contrib.staticfiles.testing import StaticLiveServerTestCase
-from selenium import webdriver
-from selenium.common.exceptions import WebDriverException
+from .base import FunctionalTest
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
-MAX_WAIT = 5
 
-class NewVisitorTest(StaticLiveServerTestCase):
-    def setUp(self):
-        self.browser = webdriver.Firefox()
-        test_server = os.environ.get('TEST_SERVER')
-
-        if test_server:
-            self.live_server_url = 'http://' + test_server
-
-    def tearDown(self) -> None:
-        self.browser.quit()
-    
-    def wait_for_row_in_list_table(self, row_text):
-        start_time = time.time()
-        
-        while True:
-            try:
-                table = self.browser.find_element(By.ID, "id_list_table")
-                rows = table.find_elements(By.TAG_NAME, "tr")
-                self.assertIn(row_text, [row.text for row in rows])
-                return
-            except (AssertionError, WebDriverException):
-                if time.time() - start_time > MAX_WAIT:
-                    raise
-                time.sleep(0.5)
-    
+class NewVisitorTest(FunctionalTest):
     def check_for_row_in_list_table(self, row_text):
         table = self.browser.find_element(By.ID, 'id_list_table')
         rows = table.find_elements(By.TAG_NAME, 'tr')
@@ -66,7 +38,6 @@ class NewVisitorTest(StaticLiveServerTestCase):
         inputbox = self.browser.find_element(By.ID,"id_new_item")
         inputbox.send_keys("Use peacock feathers to make a fly")
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
 
         # The page updates again, and now shows both items on her list
         table = self.browser.find_element(By.ID, "id_list_table")
@@ -118,27 +89,5 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
         #Satisfied, they both go back to sleep
     
-    def test_layout_and_styling(self):
-        #Edith goes to the home page,
-        self.browser.get(self.live_server_url)
+    
 
-        #her browser window is set to a very specific size
-        self.browser.set_window_size(1024,768)
-
-        #she notices the input box is nicely centered
-        inputbox = self.browser.find_element(By.ID, 'id_new_item')
-        self.assertAlmostEqual(
-            inputbox.location['x'] + inputbox.size['width'] / 2, 512, delta= 10
-        )
-
-        #she starts a new list and sees the input is nicely centered there too
-        inputbox.send_keys('testing')
-        inputbox.send_keys(Keys.ENTER)
-        self.wait_for_row_in_list_table('1: testing')
-        inputbox = self.browser.find_element(By.ID, 'id_new_item')
-        self.assertAlmostEqual(
-            inputbox.location['x'] + inputbox.size['width'] / 2, 512, delta= 10
-        )
-
-if __name__ == "__main__":
-    unittest.main()
